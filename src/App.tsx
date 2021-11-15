9742b4f035e290a9025db42e0fc00660b434e124
adilson2902 <nunojraa@hotmail.com>
2021-11-14 23:12:50 -0300

    Jutsu react using jitsi meet :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from 'react'
 
-function App() {
+import { Jutsu } from 'react-jutsu'
+
+
+const App = () => {
+  const [room, setRoom] = useState('')
+  const [name, setName] = useState('')
+  const [call, setCall] = useState(false)
+  const [password, setPassword] = useState('')
+
+  const handleClick: any = (event: any) => {
+    event.preventDefault()
+    if (room && name) setCall(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      {call ? (<Jutsu
+        roomName={room}
+        password={password}
+        displayName={name}
+        onMeetingEnd={() => console.log('Meeting has ended')}
+        loadingComponent={<p>ʕ •ᴥ•ʔ jitsi is loading ...</p>}
+        containerStyles={{ width: '100%', height: '100vh' }} />)
+        : (
+          <form>
+            <input id='room' type='text' placeholder='Room' value={room} onChange={(e) => setRoom(e.target.value)} />
+            <input id='name' type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+            <input id='password' type='text' placeholder='Password (optional)' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleClick} type='submit'>
+              Start / Join
+            </button>
+          </form>
+        )}
+     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
